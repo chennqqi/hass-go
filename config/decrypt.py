bf0fac219f3248f4441345c8537bcc9157e13553
Jurgen Kluft <jurgen.kluft@gmail.com>
2018-02-02 16:41:47 +0800

    * calendar now using JSON instead of Viper. * Shout WIP * Weather Report

--- config/decrypt.py
+++ config/decrypt.py
@@ -3,11 +3,11 @@ import pyAesCrypt
 bufferSize = 64 * 1024
 password = password = input("Enter password: ")
 # decrypt
-pyAesCrypt.decryptFile("calendar.toml.aes", "calendar.toml", password, bufferSize)
+pyAesCrypt.decryptFile("calendar.json.aes", "calendar.json", password, bufferSize)
 pyAesCrypt.decryptFile("hass.toml.aes", "hass.toml", password, bufferSize)
 pyAesCrypt.decryptFile("lighting.toml.aes",  "lighting.toml", password, bufferSize)
 pyAesCrypt.decryptFile("secrets.toml.aes", "secrets.toml", password, bufferSize)
 pyAesCrypt.decryptFile("sensors.toml.aes", "sensors.toml", password, bufferSize)
-pyAesCrypt.decryptFile("slack.toml.aes", "slack.toml", password, bufferSize)
+pyAesCrypt.decryptFile("shout.toml.aes", "shout.toml", password, bufferSize)
 pyAesCrypt.decryptFile("suncalc.toml.aes",  "suncalc.toml", password, bufferSize)
 pyAesCrypt.decryptFile("weather.toml.aes", "weather.toml", password, bufferSize)
--- config/encrypt.py
+++ config/encrypt.py
@@ -3,11 +3,11 @@ import pyAesCrypt
 bufferSize = 64 * 1024
 password = input("Enter password: ")
 # decrypt
-pyAesCrypt.encryptFile("calendar.toml", "calendar.toml.aes", password, bufferSize)
+pyAesCrypt.encryptFile("calendar.json", "calendar.json.aes", password, bufferSize)
 pyAesCrypt.encryptFile("hass.toml",     "hass.toml.aes", password, bufferSize)
 pyAesCrypt.encryptFile("lighting.toml", "lighting.toml.aes", password, bufferSize)
 pyAesCrypt.encryptFile("secrets.toml",  "secrets.toml.aes", password, bufferSize)
 pyAesCrypt.encryptFile("sensors.toml",  "sensors.toml.aes", password, bufferSize)
-pyAesCrypt.encryptFile("slack.toml",    "slack.toml.aes", password, bufferSize)
+pyAesCrypt.encryptFile("shout.toml",    "shout.toml.aes", password, bufferSize)
 pyAesCrypt.encryptFile("suncalc.toml",  "suncalc.toml.aes", password, bufferSize)
 pyAesCrypt.encryptFile("weather.toml",  "weather.toml.aes", password, bufferSize)
