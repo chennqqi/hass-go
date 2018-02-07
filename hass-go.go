4829ec82a2f073dc7046d72c11cf39a3f4dd9337
Jurgen Kluft <jurgen.kluft@gmail.com>
2018-02-07 18:19:04 +0800

    + reporter. * calendar, file or URL.

--- hass-go.go
+++ hass-go.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jurgen-kluft/hass-go/calendar"
 	"github.com/jurgen-kluft/hass-go/hass"
 	"github.com/jurgen-kluft/hass-go/lighting"
+	"github.com/jurgen-kluft/hass-go/reporter"
 	"github.com/jurgen-kluft/hass-go/sensors"
 	"github.com/jurgen-kluft/hass-go/shout"
 	"github.com/jurgen-kluft/hass-go/state"
@@ -14,80 +15,6 @@ import (
 	"github.com/jurgen-kluft/hass-go/timeofday"
 	"github.com/jurgen-kluft/hass-go/weather"
 )
-
-// This can be computed from sun-rise, sun-set
-// - sensor.dark_or_light (Dark, Twilight, Light)
-
-// The data for these sensors can also come from a calendar
-// In icloud we have the following calendars already:
-// - TimeOfDay
-// - Season
-// - Weather
-// - Lighting (NA)
-// Note: These calendars can be downloaded on a low frequency.
-// - Weather
-// - TimeOfDay (Breakfast, Morning, Lunch, Afternoon, Evening, ..)
-// - Season (Summer, Winter, Spring, Autumn)
-// - Lighting: Color-Temperature and Brightness
-
-// Every X minutes:
-// - Update Calendar
-// - Update SunCalc
-// - Update DarkOrLight
-// - Update Weather
-// - Update Lighting
-// - Update Sensors (this will HTTP to Home-Assistant)
-// - Update Notifications and Reports
-//   - Weather warnings
-//   - Weather reports
-//   - Alarms
-// - Sleep
-
-const (
-	daySeconds = 60.0 * 60.0 * 24.0
-)
-
-func hoursLater(date time.Time, h float64) time.Time {
-	return time.Unix(date.Unix()+int64(h*float64(daySeconds)/24.0), 0)
-}
-
-func buildWeatherReport(states *state.Domain) {
-	title := "Weather Report"
-
-	// Detect rain between
-	//  -  8:30 - 9:30
-	//  - 12:00 - 13:00
-	//  - 18:00 - 20:00
-	weather := states.Get("weather")
-
-	report := title + "\n"
-	report += "Change of rain is " + weather.GetStringState("currently:rain", "") + "\n"
-	//fmt.Print(report)
-
-	i := 1
-	for true {
-		key := fmt.Sprintf("hourly[%d]:", i)
-		if weather.HasTimeState(key + "from") {
-			hfrom := weather.GetTimeState(key+"from", time.Now())
-			huntil := weather.GetTimeState(key+"until", time.Now())
-			srain := weather.GetStringState(key+"rain", "")
-			scloud := weather.GetStringState(key+"clouds", "")
-			stemp := weather.GetStringState(key+"temperature", "")
-			temp := weather.GetFloatState(key+"temperature", 0.0)
-			line := fmt.Sprintf("%s, %s(%d), %s (%02d:%02d - %02d:%02d)\n", srain, stemp, int32(temp+0.5), scloud, hfrom.Hour(), hfrom.Minute(), huntil.Hour(), huntil.Minute())
-			//fmt.Print(line)
-			report += line
-		} else {
-			break
-		}
-		i++
-	}
-
-	// Temperature morning - noon - evening
-
-	// Weather report to
-	states.SetStringState("shout", "msg:weather", report)
-}
 
 func main() {
 
@@ -102,6 +29,7 @@ func main() {
 	sensorsInstance, _ := sensors.New()
 	lightingInstance, _ := lighting.New()
 	hassInstance, _ := hass.New()
+	reporterInstance, _ := reporter.New()
 
 	for true {
 		now := time.Now()
@@ -122,8 +50,7 @@ func main() {
 		lightingInstance.Process(states)
 		sensorsInstance.PublishSensors(states)
 		hassInstance.Process(states)
-
-		buildWeatherReport(states)
+		reporterInstance.Process(states)
 
 		shoutInstance.PublishMessages(states)
 
---
+++ reporter/reporter.go
@@ -0,0 +1,72 @@
+package reporter
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jurgen-kluft/hass-go/state"
+)
+
+type Instance struct {
+	history map[string]time.Time
+}
+
+func New() (c *Instance, err error) {
+	c = &Instance{}
+	return c, err
+}
+
+func (c *Instance) reportWeather(ID string, states *state.Domain) {
+	title := "Weather Report"
+
+	// Detect rain between
+	//  -  8:30 - 9:30
+	//  - 12:00 - 13:00
+	//  - 18:00 - 20:00
+	weather := states.Get("weather")
+
+	report := title + "\n"
+	report += "Change of rain is " + weather.GetStringState("currently:rain", "") + "\n"
+	//fmt.Print(report)
+
+	i := 1
+	for true {
+		key := fmt.Sprintf("hourly[%d]:", i)
+		if weather.HasTimeState(key + "from") {
+			hfrom := weather.GetTimeState(key+"from", time.Now())
+			huntil := weather.GetTimeState(key+"until", time.Now())
+			srain := weather.GetStringState(key+"rain", "")
+			scloud := weather.GetStringState(key+"clouds", "")
+			stemp := weather.GetStringState(key+"temperature", "")
+			temp := weather.GetFloatState(key+"temperature", 0.0)
+			line := fmt.Sprintf("%s, %s(%d), %s (%02d:%02d - %02d:%02d)\n", srain, stemp, int32(temp+0.5), scloud, hfrom.Hour(), hfrom.Minute(), huntil.Hour(), huntil.Minute())
+			//fmt.Print(line)
+
+			c.history[ID] = time.Now()
+			report += line
+		} else {
+			break
+		}
+		i++
+	}
+
+	// Temperature morning - noon - evening
+
+	// Weather report to
+	states.SetStringState("shout", "msg:weather", report)
+}
+
+func (c *Instance) Process(states *state.Domain) {
+
+	// In calendar these are written as:
+	//
+	//     report:weather=
+
+	reports := states.Get("report")
+	for _, r := range reports.Strings {
+		if r == "weather" {
+			id := reports.GetStringState(r+".ID", "")
+			c.reportWeather(id, states)
+		}
+	}
+}
